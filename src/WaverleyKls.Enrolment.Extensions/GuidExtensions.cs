853599e200284343bce87255639ad223390e0c83
Justin Yoo <justin.yoo@aliencube.com>
2016-12-29 20:39:48 +1100

    Add XML comments for Extensions project

--- src/WaverleyKls.Enrolment.Extensions/GuidExtensions.cs
+++ src/WaverleyKls.Enrolment.Extensions/GuidExtensions.cs
@@ -2,11 +2,20 @@
 
 namespace WaverleyKls.Enrolment.Extensions
 {
+    /// <summary>
+    /// This represents the extension entity for <see cref="Guid"/>.
+    /// </summary>
     public static class GuidExtensions
     {
+        /// <summary>
+        /// Converts the <see cref="Guid"/> value to base64-encoded. string value.
+        /// </summary>
+        /// <param name="value"><see cref="Guid"/> value to convert.</param>
+        /// <returns>Returns the base64-encoded stgring value.</returns>
         public static string ToBase64String(this Guid value)
         {
             var result = Convert.ToBase64String(value.ToByteArray());
+
             return result;
         }
     }
--- src/WaverleyKls.Enrolment.Extensions/StringExtensions.cs
+++ src/WaverleyKls.Enrolment.Extensions/StringExtensions.cs
@@ -2,14 +2,28 @@ using System;
 
 namespace WaverleyKls.Enrolment.Extensions
 {
+    /// <summary>
+    /// This represents the extension entity for <see cref="string"/>.
+    /// </summary>
     public static class StringExtensions
     {
+        /// <summary>
+        /// Checks whether the value is null or white space, or not.
+        /// </summary>
+        /// <param name="value">Value to check.</param>
+        /// <returns>Returns <c>True</c>, if the value is null or white space; otherwise returns <c>False</c>.</returns>
         public static bool IsNullOrWhiteSpace(this string value)
         {
             var result = string.IsNullOrWhiteSpace(value);
+
             return result;
         }
 
+        /// <summary>
+        /// Converts the base64-encoded string to <see cref="Guid"/>.
+        /// </summary>
+        /// <param name="base64EncodedValue">Base64-encoded string value to convert.</param>
+        /// <returns>Returns the <see cref="Guid"/> converted.</returns>
         public static Guid ToGuid(this string base64EncodedValue)
         {
             if (base64EncodedValue.IsNullOrWhiteSpace())
@@ -18,6 +32,7 @@ namespace WaverleyKls.Enrolment.Extensions
             }
 
             var result = new Guid(Convert.FromBase64String(base64EncodedValue));
+
             return result;
         }
     }
